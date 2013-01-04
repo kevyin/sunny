9b0838a3748bf59a3151c09238ca3d1d0c644811
Kevin Ying <k.ying@garvan.org.au>
2013-01-04 17:04:12 +1100

    bug fix

--- sge_is_running.sh
+++ sge_is_running.sh
@@ -3,8 +3,13 @@
 res=0
 for id in "$@"
 do
-    qstat -j $1 -s r &> /dev/null 
-    res=$(($res + $?))
+    qstat -j $id -s r &> /dev/null 
+    if [ $? -eq 0 ]
+    then
+        echo 0
+        exit 0
+    fi
 done
 
-echo $res
+echo 1
+exit 1
--- sge_wait_on_job.sh
+++ sge_wait_on_job.sh
@@ -6,7 +6,7 @@ echo $_jobs
 is_running=0
 while [ $is_running -eq 0 ]
 do
-    is_running=`~/dev/sunny/is_running.sh ${_jobs[@]}`
+    is_running=`~/dev/sunny/sge_is_running.sh ${_jobs[@]}`
     #echo $is_running
     sleep 1
 done
