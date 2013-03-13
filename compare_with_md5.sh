f1099a4371bb5b43e84a23f7380cdf7a52256b6b
Kevin Ying <k.ying@garvan.org.au>
2013-03-13 16:08:43 +1100

    use a ~/tmp for tmp files

--- compare_with_md5.sh
+++ compare_with_md5.sh
@@ -3,7 +3,10 @@
 md5file=$1
 file=$2
 
-md5sum $file | cut -f1 -d' ' > ${file}.tmp1 
-cut -f1 -d' ' ${md5file} > ${file}.tmp2 
-diff ${file}.tmp1 ${file}.tmp2 
-rm ${file}.tmp1 ${file}.tmp2
+tmp1=~/tmp/${file##*/}.tmp1
+tmp2=~/tmp/${file##*/}.tmp2
+
+md5sum $file | cut -f1 -d' ' > $tmp1
+cut -f1 -d' ' ${md5file} > $tmp2 
+diff $tmp1 $tmp2 
+#rm $tmp1 $tmp2
